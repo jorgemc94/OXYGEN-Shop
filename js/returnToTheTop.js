2391424a061a4fcd4b13b57e0769339e49fe4d5b
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-24 20:43:44 +0200

    Change error

--- js/returnToTheTop.js
+++ js/returnToTheTop.js
@@ -3,7 +3,7 @@
     const btn_Up = document.getElementById('button_Up');
 
     //Evento de escucha cuando hacemos click a nuestro boton con id btn_Up
-    btn_Up.addEventListener("click", () => {
+    btn_Up.addEventListener("click", ()=> {
         //Funcion de temporizar, se ejecutará 0.2 segundos despues de hacer click al botom
         setTimeout(() => {
             //Metodo que especifica que la ventana se desplaza hacia la parte superior de la pagina de forma suave
