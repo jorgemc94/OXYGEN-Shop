6ed511559833300bea852741d3664839914f1e7e
jorgemc94 <jmaciasc1994@gmail.com>
2024-04-24 17:07:30 +0200

    coinSelector

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,56 +1,64 @@
-//Validación formulario
 
-    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    let inputName = document.getElementById('name');
-    let inputEmail = document.getElementById('email');
-    let inputCheckbox = document.getElementById('check');
-
-    //Funcion validacion nombre
-
-    const validName = () => {
-        // Si la longitud del valor introducido es menor a 2 o mayor a 100
-        if(inputName.value.length <= 2 || inputName.value.length > 100) {
-            //Asignamos el borde del input de color rojo
-            inputName.style.border = '0.2rem solid red';
-            return false;
+    const form = document.getElementById('form');
+    form.addEventListener('submit', (event) => {
+        const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        const inputName = document.getElementById('name');
+        const userName = inputName.value;
+        const inputEmail = document.getElementById('email');
+        const userEmail = inputEmail.value;
+        const inputCheckbox = document.getElementById('check');
+        let checkedCorrect = false;
+        let nameCorrect = false;
+        let emailCorrect = false;
+        event.preventDefault();
+        if (userName.length < 2 || userName.length > 100) {
+            inputName.style.cssText = 'border-bottom: 0.2rem solid red';
+            alert ('Enter name valid');
+            
         } else {
-            //Asignamos el borde del input de color verde
-            inputName.style.border = '0.2rem solid green';
-            return true;
+            inputName.style.cssText = '';
+            inputName.value = '';
+            nameCorrect = true;
         }
-    }
-    inputName.onchange = () => validName(); //Asignamos el evento onchange a nuestro input y cada vez que cambie su estado llama a la funcion validName();
-    //Funcion validación email
 
-    const validEmail = () => {
-        //Cuando el valor del inputEmail sea diferente a la variable con la expresion regular
-        if(!regex.test(inputEmail.value)) {
-            //Asignamos el borde del input de color rojo
-            inputEmail.style.border = '0.2rem solid red';
-            return false
+        if (!regex.test(userEmail)) {
+            inputEmail.style.cssText = 'border-bottom: 0.2rem solid red';
+            alert ('Enter mail valid');
         } else {
-            //Asignamos el borde del input de color verde
-            inputEmail.style.border = '0.2rem solid green';
-            return true
+            inputEmail.style.cssText = '';
+            inputEmail.value = '';
+            emailCorrect = true;
         }
-    }
-    inputEmail.onchange = () => validEmail(); //Asignamos el evento onchange a nuestro inputEmail y cada vez que cambie su estado llama a la funcion validEmail();
 
-    //Funcion validacion checkbox
-
-    const validCheck = () => {
-        //Si nuestro check es distinto de checked
-        if(!inputCheckbox.checked) {
-            //Asignamos el borde del input de color rojo
-            inputCheckbox.style.border = '0.2rem solid red';
-            return false;
+        if (!inputCheckbox.checked) {
+            inputCheckbox.style.cssText = 'border: 0.2rem solid red';
+            alert ('accept terms and conditions');
         } else {
-            //Dejamos nuestro input por defecto
-            inputCheckbox.style.border = '';
-            return true;
+            inputCheckbox.style.cssText = '';
+            inputCheckbox.checked = false;
+            checkedCorrect = true;
         }
-    }
 
-    inputCheckbox.onchange = () => validCheck();//Asignamos el evento onchange a nuestro check y cada vez que cambie su estado llama a la funcion validCheck();
 
-//Recoger los datos del formulario y mandarselos a una API
+        const url = 'https://jsonplaceholder.typicode.com/posts';
+        if (nameCorrect && emailCorrect && checkedCorrect) {
+        fetch(url, {
+            method: 'POST',
+            body: {
+                name: userName,
+                email: userEmail
+            }
+        }).then((response) => {
+            if (response.ok) {
+                response.json()
+                .then((json) => {
+                    console.log(json);
+                    alert ('User add successful');
+                })
+                .catch((error) => console.log(error)); 
+            }
+        })
+        }
+    })
+
+   
